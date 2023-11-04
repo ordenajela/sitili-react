1746a378323894e4eb5700e5db536a10b8cf03ef
LuisBrito1687 <124101529+LuisBrito1687@users.noreply.github.com>
2023-11-04 14:24:49 -0600

    Se agrega la grid de productos para vistas en general

--- src/assets/views/productos.js
+++ src/assets/views/productos.js
@@ -1,13 +1,15 @@
 import PrimarySearchAppBar from "../components/navbar2";
 import CarouselCategories from "../components/carousel-categories";
 import FilterTags from "../components/filter-tags";
+import ResponsiveGridWithPagination from "../components/products";
 
 function Productos() {
     return (
       <div>
         <PrimarySearchAppBar />
-        <CarouselCategories />
+        {/* <CarouselCategories /> */}
         <FilterTags />
+        <ResponsiveGridWithPagination />
         <h1>Vista Principal Brito ya aqui que se vea los productos o como lo quieras llevar</h1>
       </div>
     );
