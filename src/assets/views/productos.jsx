b1dff885bd877158180cc5a9eed6bb16cfd7af85
LuisBrito1687 <124101529+LuisBrito1687@users.noreply.github.com>
2023-11-07 16:43:00 -0600

    Se agrega el footer general y se cambia la extensión de los archivos js a jsx

--- src/assets/views/productos.jsx
+++ src/assets/views/productos.jsx
@@ -2,6 +2,7 @@ import PrimarySearchAppBar from "../components/navbar2";
 import CarouselCategories from "../components/carousel-categories";
 import FilterTags from "../components/filter-tags";
 import ResponsiveGridWithPagination from "../components/products";
+import StickyFooter from "../components/footer";
 
 function Productos() {
     return (
@@ -10,7 +11,7 @@ function Productos() {
         {/* <CarouselCategories /> */}
         <FilterTags />
         <ResponsiveGridWithPagination />
-        <h1>Vista Principal Brito ya aqui que se vea los productos o como lo quieras llevar</h1>
+        <StickyFooter />
       </div>
     );
   }
